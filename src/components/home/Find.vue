ff503f8627a2129245fa15e4f2a1483069f1119d
you <you80072@163.com>
2018-07-23 16:32:22 +0800

    3

--- src/components/home/Find.vue
+++ src/components/home/Find.vue
@@ -1,9 +1,6 @@
 <template>
 <div>
-    <div class="top">
-        
-        
-    </div>
+    
 
     
 </div>
@@ -29,36 +26,5 @@ export default {
 </script>
 
 <style scoped>
-h1{
-    font-size: 20px;
-    height: 1000px;
-    background: gold;
-}
-.top {
-    width: 100%;
-    height: .66rem;
-    background: fuchsia;
-    border-top : 1px solid #e6e6e6;
-    border-bottom:1px solid #e6e6e6;
-    overflow: hidden;
-}
-.a{
-    width: 100%;
-    height: .8rem;
 
-    overflow-x:scroll;
-    overflow-y: hidden;
-}
-.top ul {
-    width: 1000px;
-    display: flex;
-    flex-wrap: nowrap;
-    overflow-y: hidden;
-}
-.top-list{
-    margin-left: .4rem;
-    height: 100%;
-    line-height: .66rem;
-    font-size: .28rem;
-}
 </style>
